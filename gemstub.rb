a0eed1cc61375ad304a1ea5b40aeaa2ab7080623
dearblue <dearblue@users.noreply.github.com>
2017-03-03 21:40:02 +0900

    リンクの修正

--- gemstub.rb
+++ gemstub.rb
@@ -6,10 +6,10 @@ GEMSTUB = Gem::Specification.new do |s|
 This is a C extention for "crc" gem library.
 Just install this, and to do "require 'crc'" only. Additional other work is not required.
 EOS
-  s.homepage = "https://osdn.jp/projects/rutsubo/"
+  s.homepage = "https://github.com/dearblue/ruby-crc-turbo"
   s.licenses = ["BSD-2-Clause", "CC0-1.0"]
   s.author = "dearblue"
-  s.email = "dearblue@users.osdn.me"
+  s.email = "dearblue@users.noreply.github.com"
 
   s.required_ruby_version = ">= 2.0"
   s.add_development_dependency "rake"
