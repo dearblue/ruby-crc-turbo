be13e2c8b01c843c450ec03283e7e53dc54bb8e2
dearblue <dearblue@users.noreply.github.com>
2017-03-05 01:06:56 +0900

    バージョン関連の修正

--- README.md
+++ README.md
@@ -12,12 +12,12 @@ Just install this, and to do "require 'crc'" only. Additional other work is not 
   * author: dearblue (mailto:dearblue@users.noreply.github.com)
   * report issue to: <https://github.com/dearblue/ruby-crc/issues>
   * how to install: ``gem install crc-turbo``
-  * version: 0.3
+  * version: 0.4
   * production quality: TECHNICAL PREVIEW
   * licensing:
       * ***BSD-2-Clause : MAIN LICENSE***
       * Creative Commons License Zero (CC0 / Public Domain) : ``ext/crc/crc_imps.h``
-  * dependency gems: crc-0.3 (<https://rubygems/gems/crc>)
+  * dependency gems: crc-0.4 (<https://rubygems/gems/crc>)
   * dependency external C libraries: none
   * bundled external C libraries: none
 
--- gemstub.rb
+++ gemstub.rb
@@ -1,19 +1,20 @@
 GEMSTUB = Gem::Specification.new do |s|
   s.name = "crc-turbo"
-  s.version = "0.3"
-  s.summary = "general CRC generator"
+  s.version = "0.4"
+  s.summary = "C written accelerator for \"crc\" gem library"
   s.description = <<EOS
-This is a C extention for "crc" gem library.
-Just install this, and to do "require 'crc'" only. Additional other work is not required.
+C written accelerator for "crc" gem library.
+Just install this, and to do ``require "crc"`` only.
+Additional other work is not required.
 EOS
   s.homepage = "https://github.com/dearblue/ruby-crc-turbo"
   s.licenses = ["BSD-2-Clause", "CC0-1.0"]
   s.author = "dearblue"
   s.email = "dearblue@users.noreply.github.com"
 
-  s.required_ruby_version = ">= 2.0"
+  s.required_ruby_version = ">= 2.2"
   s.add_development_dependency "rake"
-  s.add_runtime_dependency "crc", "~> 0.3"
+  s.add_runtime_dependency "crc", "~> 0.4"
 end
 
 EXTMAP["crc"] = "crc/_turbo"
